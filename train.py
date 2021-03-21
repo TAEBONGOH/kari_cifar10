e44fd493994282ea7484a029f495883aebd7dae0
TAEBONG OH <tboh@kari.re.kr>
2021-03-21 04:27:09 +0000

    trainning - no gpu

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import torchvision
 from models.vgg import vgg11_bn
 import torch.nn as nn
 import torch.optim as optim
+import time
 
 def train(opt):
     epochs = opt.epochs
@@ -27,9 +28,29 @@ def train(opt):
     loss_fn = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
     
+    start_epoch, end_epoch = (0, epochs)
+    for epoch in range(start_epoch, end_epoch):
+        print('epoch: %d/%d' % (epoch, end_epoch-1))
+        t0 = time.time()
+        epoch_loss = train_one_epoch(train_dataloader, model, loss_fn, optimizer)
+        t1 = time.time()
+        print('loss=%.4f (took $.2f sec)\n' % (epoch_loss, t1-t0))
+
+def train_one_epoch(train_dataloader, model, loss_fn, optimizer):
+    model.train()
+    losses = []
     for i, (imgs, targets) in enumerate(train_dataloader):
-        print('batch idx=%d/%d' % (i, len(train_dataloader)-1))
-        pass # debug checkpoint
+        preds = model(imgs)      # forward
+        loss = loss_fn(preds, targets) # calculates the iteration loss
+        optimizer.zero_grad()  # zeros the parameter gradients
+        loss.backward()        # backward
+        optimizer.step()       # update weights
+
+        # print the iteration loss every 100 iterations
+        if i % 100 == 0:
+            print('\t iteration: %d/%d, loss=%.4f' % (i, len(train_dataloader)-1, loss))
+        losses.append(loss.item())
+    return torch.tensor(losses).mean().item()
 
 
 if __name__ == '__main__':
